f9459205e129eae1a24f04c612e1aae4bcadef69
jyotiprokash <jyotiprokash2000@gmail.com>
2024-11-01 00:31:39 +0600

    added react setup,file,navbar,footer,etc.

---
+++ src/main.jsx
@@ -0,0 +1,36 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Root from './Components/Root/Root';
+import Error from './Components/Error Page/Error';
+import Home from './Components/Home/Home';
+import Dashboard from './Components/Dashboard/Dashboard';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root></Root>,
+    errorElement: <Error></Error>,
+    children:[
+      {
+        path: "/",
+        element:<Home></Home>
+      },
+      {
+        path: "dashboard",
+        element: <Dashboard></Dashboard>
+      }
+    ]
+  },
+]);
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>,
+)
